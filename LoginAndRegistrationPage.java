472df4afd624ab3cb2006cdf9bcbc1e52e10f7fd
Rafig Asgarli <101342862+raskerli@users.noreply.github.com>
2024-04-24 20:16:20 +0400

    Update Setup and Initialization

--- LoginAndRegistrationPage.java
+++ LoginAndRegistrationPage.java
@@ -1,1 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
+public class UserAdmin extends JFrame {
+
+    private Map<String, String> userDatabase; 
+
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+
+    public UserAdmin() {
+        super("Login & Registration");
+      
+        userDatabase = new HashMap<>();
+        userDatabase.put("admin", "admin");
