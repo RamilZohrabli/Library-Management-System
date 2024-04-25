b3a2aba268c8a56355d55d2a268a5685a4e42c7a
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-04-25 12:51:50 +0400

    General datanase with its attributes and constructor

---
+++ GeneralDatabase.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class GeneralDatabase{
+    private String author;
+    private String title;
+    private double averageRating;
+    private int countRating;
+    private List<String> reviews;
+    //Constructor
+    public GeneralDatabase(String title, String author){
+        this.title = title;
+        this.averageRating = 0.0;
+        this.countRating = 0;
+        if(author == null){
+            this.author = "Unknown";
+        }
+        else{
+            this.author = author;
+        }
+        this.reviews = new ArrayList<>();
+    }
+}
