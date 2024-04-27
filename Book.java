0975c7e26751c9d9c6ca2d47af4af6dbffaee14a
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-04-27 21:23:43 +0400

    Constructor

--- Book.java
+++ Book.java
@@ -1,8 +1,26 @@
-import java.util.List;
+import java.util.*;
 
 public class Book {
+    //Attributes
     private String title;
     private String author;
     private Float averageRating;
     private List<String> userReviews;
+    //Constructor
+    public Book(String title, String author){
+        if(title != null){
+            this.title = title;
+        }
+        else{
+            this.title = "Unknown";
+        }
+        if(author != null){
+            this.author = author;
+        }
+        else{
+            this.author = "Unknown";
+        }
+        this.averageRating = null;
+        this.userReviews = new ArrayList<>();
+    }
 }
