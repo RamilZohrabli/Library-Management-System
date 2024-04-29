85c1c05d43ddfd49d8b560cf4c492e8ef7a93543
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-04-29 12:27:03 +0400

    GeneralDatabase class, its attribute and constructor

---
+++ GeneralDatabase.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class GeneralDatabase{
+    private List<GeneralBook> books;
+    
+    public GeneralDatabase(){
+        books = new ArrayList<>();
+    }
+}
